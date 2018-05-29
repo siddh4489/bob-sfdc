339618ad739d4d2dfe8b843f79fd6cae626d67c2
siddh4489 <siddh4489@gmail.com>
2018-05-29 14:26:20 +0530

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE.txt in the project root for license information.
 var express = require('express');
 var router = express.Router();
+var nforce = require('nforce');
 var authHelper = require('../helpers/auth');
 var graph = require('@microsoft/microsoft-graph-client');
 var mailto;
@@ -151,301 +152,21 @@ router.get('/', async function (req, res, next) {
     Date.prototype.format = function (mask, utc) {
         return dateFormat(this, mask, utc);
     };
-
-
-
-    var rooms = [{
-            "room": "chime",
-            "name": "cr.pneb2.2.chime.4",
-            "address": "cr.pneb2.2.chime.4@symantec.com"
-        },
-        {
-            "room": "clarinet",
-            "name": "CR.PNEB2.2.Clarinet.10",
-            "address": "cr.pneb2.2.clarinet.10@symantec.com"
-        },
-        {
-            "room": "conga",
-            "name": "CR.PNEB2.2.Conga.8",
-            "address": "cr.pneb2.2.conga.8@symantec.com"
-        },
-        {
-            "room": "melodica",
-            "name": "CR.PNEB2.2.Melodica.16(VC)",
-            "address": "cr.pneb2.2.melodica.16@symantec.com"
-        }
-    ];
-
-    let parms = {
-        title: 'event',
-        active: {
-            event: true
-        }
-    };
+   
+    ;
 
     const accessToken = await authHelper.getAccessToken(req.cookies, res);
     const userName = req.cookies.graph_user_name;
 
     if (accessToken && userName) {
-        parms.user = userName;
-
-        // Initialize Graph client
-        const client = graph.Client.init({
-            authProvider: (done) => {
-                done(null, accessToken);
-            }
-        });
-
-        try {
-
-
-            // Get the first 10 contacts in alphabetical order
-            // by given name
-            console.log('----- String ---' + req.param('person'));
-            var reserveKey = ["with", "subject", "body", "room", "time"];
-            var keyPoint = new Object();
-            var finalMap = new Object();
-            var myString = req.param('person');
-            var stage = req.param('stage');
-
-            if (stage == 'Initial') {
-                this.mailto = null;
-                this.mailsubject = null;
-                this.mailbody = null;
-                this.personName = null;
-                this.roomadd = null;
-                this.roomname = null;
-                this.starttime = null;
-                this.endtime = null;
-            }
-
-            console.log('----stage---' + stage);
-            for (var i = 0; i < reserveKey.length; i++) {
-                if (myString.lastIndexOf(reserveKey[i]) != -1) {
-                    keyPoint[reserveKey[i]] = myString.lastIndexOf(reserveKey[i]) + reserveKey[i].length;
-                }
-            }
-
-
-            var map1 = sortProperties(keyPoint);
-            for (var key in map1) {
-                finalMap[String(map1[key]).split(',')[0]] = myString.slice(String(map1[key]).split(',')[1], (map1[++key] == undefined ? myString.length : String(map1[key]).split(',')[1] - String(map1[key]).split(',')[0].length));
-            }
-
-            var resultData = '<Html><table style="width:100%;border:1px solid black;">';
-            var emailSearch = finalMap['with'];
-
-            console.log('-- email search--' + emailSearch);
-
-
-            //var mailto;
-            //var mailsubject;
-            //var mailbody;
-            var bobmsg;
-
-            if (emailSearch !== undefined && emailSearch != ' ') {
-
-                console.log('-- email search 1--' + emailSearch);
-                const result = await client
-                    .api('/me/people/?$search=' + emailSearch)
-                    .version("beta")
-                    .top(1)
-                    .get();
-
-                if (result.value[0] !== undefined) {
-                    console.log('--out mail --' + stage);
-                    console.log('--out mail 1--' + this.mailto);
-                    if (result.value.length > 1) {
-                        for (i = 0; i < result.value.length; i++) {
-                            console.log('-- search result-->' + result.value[i].displayName);
-                            if ((result.value[i].displayName).includes(emailSearch)) {
-                                console.log('-- search result-->' + result.value[i].userPrincipalName);
-                                this.personName = result.value[i].displayName;
-                                this.mailto = result.value[i].userPrincipalName;
-                            }
-
-                        }
-                    } else {
-                        this.personName = result.value[0].displayName;
-                        this.mailto = result.value[0].userPrincipalName;
-                    }
-
-
-                    console.log('--in mail 1--');
-                    //this.mailto = result.value[0].userPrincipalName;
-                    console.log('--in mail 1--' + this.mailto);
-                    //this.personName = result.value[0].displayName;	    
-
-                    //resultData+= '<tr><td>To:</td><td>'+result.value[0].userPrincipalName+'</td></tr>';
-                }
-                //console.log('---->'+result.value[0].userPrincipalName);	    
-            }
-
-
-
-
-
-
-            /* const result1 = await client
-             .api('/me/events')
-             .post(event, (err, res) => {
-               console.log(JSON.stringify(err)+'Event Response -> '+JSON.stringify(res));
-              });*/
-
-            console.log('wow-- this.mailto--' + this.mailto);
-
-            if (this.mailto != undefined) {
-                resultData += '<tr><td>To:</td><td>' + this.mailto + '</td></tr>';
-            } else {
-                bobmsg = 'Tell me email address';
-            }
-
-            console.log('wow-- this.mailsubject--' + this.mailsubject);
-
-            if (finalMap['subject'] != undefined) {
-                this.mailsubject = finalMap['subject'];
-            }
-            console.log('wow-- this.room--' + finalMap['room']);
-            if (finalMap['room'] != undefined) {
-                for (i = 0; i < rooms.length; i++) {
-                    console.log(finalMap['room'] + '----wow-- rooms[i].name--' + rooms[i].room);
-                    console.log(finalMap['room'].length + '----wow-- rooms[i].name--' + (rooms[i].room).length);
-
-                    if (' ' + rooms[i].room === finalMap['room']) {
-                        console.log('-- in room--' + rooms[i].address);
-                        this.roomadd = rooms[i].address;
-                        this.roomname = rooms[i].name;
-                    }
-                }
-            }
-
-
-
-            if (this.mailsubject != undefined) {
-                console.log('--out subject 1--' + stage);
-                console.log('--out subject 2--' + finalMap['subject']);
-                resultData += '<tr><td>Subject:</td><td>' + this.mailsubject + '</td></tr>';
-                console.log('--in subject 2--' + resultData);
-            } else {
-                bobmsg = 'Please Help me with Subject line.'
-            }
-
-            if (finalMap['body'] != undefined) {
-                this.mailbody = finalMap['body'];
-            }
-            if (this.mailbody != undefined) {
-                resultData += '<tr><td>Body:</td><td>' + this.mailbody + '</td></tr>';
-            }
-
-            if (this.roomadd != undefined) {
-                resultData += '<tr><td>Conference Room:</td><td>' + this.roomname + '</td></tr>';
-                console.log('--room--' + this.roomname);
-            } else {
-                if (bobmsg == undefined) {
-                    bobmsg = 'Please Select Conference Room for meeting.';
-                }
-            }
-
-            if (finalMap['time'] != undefined) {
-                console.log('----- time ----' + finalMap['time']);
-                var splitData = finalMap['time'].substr(1).split(' ');
-                var day = splitData[0].match(/\d+/g).map(Number);
-                var month = splitData[1];
-                var year = (new Date()).getFullYear();
-                var fromTime, toTime;
-                if (splitData[2].toUpperCase().indexOf('P') != -1)
-                    fromTime = 12 + Number(splitData[2].match(/\d+/g).map(Number));
-                else
-                    fromTime = splitData[2].match(/\d+/g).map(Number);
-
-                if (splitData[4].toUpperCase().indexOf('P') != -1)
-                    toTime = 12 + Number(splitData[4].match(/\d+/g).map(Number));
-                else
-                    toTime = splitData[5].match(/\d+/g).map(Number);
-                var now = new Date(month + ' ' + day + ', ' + year + ' ' + fromTime + ':00:00');
-                this.starttime = now.format("isoDateTime");
-                now = new Date(month + ' ' + day + ', ' + year + ' ' + toTime + ':00:00');
-                this.endtime = now.format("isoDateTime");
-                console.log(this.starttime + '----time---' + this.endtime);
-            }
-            if (this.starttime != undefined) {
-                resultData += '<tr><td>Start Time:</td><td>' + this.starttime + '</td></tr>';
-                resultData += '<tr><td>End Time:</td><td>' + this.endtime + '</td></tr>';
-            } else {
-                if (this.roomadd != undefined) {
-                    bobmsg = 'Please specify Start time and End time.';
-                }
-            }
-
-            var event = {
-                "subject": (this.mailsubject != undefined ? this.mailsubject : ''),
-                "body": {
-                    "contentType": "HTML",
-                    "content": (this.mailbody != undefined ? this.mailbody : '')
-                },
-                "start": {
-                    "dateTime": (this.starttime != undefined ? this.starttime : ''),
-                    "timeZone": "Pacific Standard Time"
-                },
-                "end": {
-                    "dateTime": (this.endtime != undefined ? this.endtime : ''),
-                    "timeZone": "Pacific Standard Time"
-                },
-                "location": {
-                    "displayName": (this.roomname != undefined ? this.roomname : '')
-                },
-                "attendees": [{
-                        "emailAddress": {
-                            "address": (this.mailto != undefined ? this.mailto : ''),
-                            "name": (this.personName != undefined ? this.personName : '')
-                        },
-                        "type": "required"
-
-                    },
-                    {
-                        "emailAddress": {
-                            "address": (this.roomadd != undefined ? this.roomadd : ''),
-                            "name": (this.roomname != undefined ? this.roomname : '')
-                        }
-                    }
-                ]
-            }
-
-            console.log('Event Json----->' + JSON.stringify(event));
-
-            if (stage == 'ready to send' && (myString === 'send' || myString === 'yes')) {
-                const result1 = await client
-                    .api('/me/events')
-                    .post(event, (err, res) => {
-                        console.log(JSON.stringify(err) + 'Event Response -> ' + JSON.stringify(res));
-                    });
-
-                bobmsg = 'meeting set successfully with ' + this.personName + '. Have a good day';
-                stage = 'Initial';
-                this.mailto = null;
-                this.mailbody = null;
-                this.mailsubject = null;
-                this.starttime = null;
-                this.endtime = null;
-                this.roomname = null;
-                this.roomadd = null;
-
-                resultData = 'Meeting Set Successfully';
-            }
-            if (bobmsg == undefined) {
-                bobmsg = 'Mail is ready to Send. Are you sure you want to send ?';
-                stage = 'ready to send';
-            }
-
-
-            resultData += '</table></html>';
-            if (stage == 'Initial') {
-                stage = 'in progress';
-            }
+       
+            var bobmsg;            
+            var resultData += '</table></html>';
+           
             res.status(200).json({
                 bob: bobmsg,
                 consoleoutput: resultData,
-                state: stage
+                state: 
             });
 
         } catch (err) {
@@ -460,7 +181,6 @@ router.get('/', async function (req, res, next) {
         }
 
     } else {
-        // Redirect to home
         res.redirect('/');
     }
 });
