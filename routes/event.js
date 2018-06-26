d7ac6acf3e078a6f9243698748e3a784395b3652
siddh4489 <siddh4489@gmail.com>
2018-06-26 15:33:19 +0530

    Update event.js

--- routes/event.js
+++ routes/event.js
@@ -169,9 +169,9 @@ router.get('/', async function (req, res, next) {
                  console.log('---Opportunity List--->'+JSON.stringify(resp.records));
                  //res.send(resp.records);
                  var resultData = '<Html><table style="width:100%;border:1px solid black;">';
-                     resultData += '<tr><th>Opportunity Name</th><th>Amount</th><th>Stage</th><th>Close Date</th></tr>';
+                     resultData += '<tr><th>Sr No</th><th>Opportunity Name</th><th>Amount</th><th>Stage</th><th>Close Date</th></tr>';
                  for(var i = 0; i < resp.records.length; i++){
-                      
+                      resultData += '<tr><td>i+1</td><td>resp.records[i].name</td><td>resp.records[i].amount</td><td>resp.records[i].stage</td><td>resp.records[i].closedate</td></tr>';
                  }
                   resultData += '</table></Html>';
                   res.status(200).json({
